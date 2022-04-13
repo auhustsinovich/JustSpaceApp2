40658574a3c67d743054bcd75446229496e294b0
auhustsinovich <avgustinovich4@gmail.com>
2022-04-13 16:54:27 +0200

    Initial Commit

---
+++ JustSpaceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustSpaceApp
+//
+//  Created by Daniil on 13.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
